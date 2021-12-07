4dabfe6e94db5bfac493641c553a13107c8e22d7
binbin <binbinyouli_2010@163.com>
2021-12-07 21:57:19 +0800

    新建maven聚合工程，并引入springboot包

---
+++ auhushop-api/src/main/java/com/binbinxiu/aihushop/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.binbinxiu.aihushop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * aihushop  api启动类
+ *
+ * @author binbin
+ * @date 2021/12/7 19:34
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}

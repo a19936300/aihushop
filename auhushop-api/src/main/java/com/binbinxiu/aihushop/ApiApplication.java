9b9c279a7fcf1a0e7ed05c2d258cf6cb75398096
binbin <binbinyouli_2010@163.com>
2021-12-12 19:36:39 +0800

    配置h2数据库和mysql数据库连接

--- auhushop-api/src/main/java/com/binbinxiu/aihushop/ApiApplication.java
+++ auhushop-api/src/main/java/com/binbinxiu/aihushop/ApiApplication.java
@@ -18,19 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/12/7 19:34
  */
 @SpringBootApplication
-@Slf4j
-@RestController
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class,args);
-    }
-
-    @GetMapping
-    public void get(){
-        log.info("这是 info 日志");
-        log.error("这是 error 日志");
-        log.debug("这是 debug 日志");
-        log.warn("这是 warn 日志");
-        log.trace("这是 啥 日志");
     }
 }

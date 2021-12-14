d6b854fc43c4abbefcbeab9055ac08da5a1f31b9
binbin <binbinyouli_2010@163.com>
2021-12-14 10:18:11 +0800

    修改pom为spring-webflux

--- aihushop-api/src/main/java/com/binbinxiu/aihushop/controller/HelloWorldController.java
+++ aihushop-api/src/main/java/com/binbinxiu/aihushop/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * TODO
@@ -16,8 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloWorldController {
     @GetMapping
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello SpringFox!");
+    public Mono<String> hello() {
+        return Mono.just("Hello SpringFox!");
     }
 
     @GetMapping("/double")

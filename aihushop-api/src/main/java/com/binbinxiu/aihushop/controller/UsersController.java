4e70e41861da5954366e23a685407d8c9fb6d69e
binbin <binbinyouli_2010@163.com>
2021-12-14 18:26:06 +0800

    前端控制器提交

--- aihushop-api/src/main/java/com/binbinxiu/aihushop/controller/UsersController.java
+++ aihushop-api/src/main/java/com/binbinxiu/aihushop/controller/UsersController.java
@@ -1,20 +1,23 @@
 package com.binbinxiu.aihushop.controller;
 
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 用户表  前端控制器
  * </p>
  *
  * @author binbin
- * @since 2021-12-13
+ * @since 2021-12-14
  */
 @RestController
-@RequestMapping("/user")
-public class UserController {
+@RequestMapping("/users")
+@Api("用户表  前端控制器")
+public class UsersController {
+
 
 }
